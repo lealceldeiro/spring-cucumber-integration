1278a35760990c68947236a5a69a5afd57060b38
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2020-09-09 16:28:59 +0300

    Add missing files from previous commit

---
+++ src/test/java/com/example/hello/cucumber/handler/MockMvcHandler.java
@@ -0,0 +1,49 @@
+package com.example.hello.cucumber.handler;
+
+import com.example.hello.Payload;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Component
+@Scope("prototype")
+public class MockMvcHandler {
+    private final ObjectMapper objectMapper;
+    private final MockMvc mockMvc;
+
+    private String greeting;
+
+    public MockMvcHandler(ObjectMapper objectMapper, WebApplicationContext context) {
+        this.objectMapper = objectMapper;
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).alwaysDo(print()).apply(springSecurity()).build();
+    }
+
+    public void sayName(String name) throws Exception {
+        String payload = objectMapper.writeValueAsString(new Payload(name));
+
+        mockMvc
+                .perform(post("/name").contentType(MediaType.APPLICATION_JSON_UTF8).content(payload))
+                .andExpect(status().isOk());
+    }
+
+    public void sayHello() throws Exception {
+        greeting = mockMvc
+                .perform(get("/salute"))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+    }
+
+    public String returnedGreeting() {
+        return greeting;
+    }
+}
